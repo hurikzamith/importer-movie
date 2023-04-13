ff4900ef684c4a8e9f86c3c955d2e3ec61e4e7eb
hurik zamith <hurikzamith@gmail.com>
2023-04-13 19:49:45 -0300

    seed functional, importing from api in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = "https://movie-catalog-api-v2.herokuapp.com/"
+
+  puts "Importing movies from page "
+  movies = JSON.parse(URI.open(url).read)
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    Movie.create(
+      id: movie['id'].to_i,
+      title: movie['title'],
+      genre: movie['genre'],
+      year: movie['year'],
+      description: movie['description'],
+      country: movie['country']
+    )
+  end
+puts "Movies created"
