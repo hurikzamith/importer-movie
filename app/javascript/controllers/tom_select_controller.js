5c28ffb288f8eba1a3b7c1b190a486ab9758f6af
hurik zamith <hurikzamith@gmail.com>
2023-04-14 15:43:47 -0300

    movies on index and with show

---
+++ app/javascript/controllers/tom_select_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import TomSelect from "tom-select";
+
+export default class extends Controller {
+  // ...
+
+  connect() {
+    new TomSelect(
+      this.element ,
+      this.optionsValue
+    );
+  }
+}
